0457bd4b8979e50ee2ef1a1513df45cc04e4c029
Rajanand <75003063+beyond-space21@users.noreply.github.com>
2022-01-15 10:22:17 +0530

    Update art.js

--- art.js
+++ art.js
@@ -1,14 +1,17 @@
-function getCentre(ele, axis) {
+function gettoCentre(ele, axis) {
     if (axis === 'y')
-        ele.style.marginTop = (window.innerHeight - ele.offsetHeight) / 2 + 'px'
+        ele.style.marginTop = (window.innerHeight - ele.offsetHeight-15) / 2 + 'px'
     else if (axis === 'x')
-        ele.style.marginLeft = (window.innerWidth - ele.offsetWidth) / 2 + 'px'
+        ele.style.marginLeft = (window.innerWidth - ele.offsetWidth-15) / 2 + 'px'
     else {
-        ele.style.marginTop = (window.innerHeight - ele.offsetHeight) / 2 + 'px'
-        ele.style.marginLeft = (window.innerWidth - ele.offsetWidth) / 2 + 'px'
+        ele.style.marginTop = (window.innerHeight - ele.offsetHeight-15) / 2 + 'px'
+        ele.style.marginLeft = (window.innerWidth - ele.offsetWidth-15) / 2 + 'px'
     }
 }
-
+function getCentreTo(ele,x,y){
+    ele.style.marginTop = (y - ele.offsetHeight/2) + 'px'
+    ele.style.marginLeft = (x - ele.offsetWidth/2) + 'px'
+}
 function cent(cent, elm) {
     var a = (cent / 100) * elm.offsetHeight
     var b = (cent / 100) * elm.offsetWidth
@@ -22,4 +25,12 @@ function pixtoint(elm) {
 }
 function map(x, in_min, in_max, out_min, out_max) {//https://arduino.stackexchange.com/questions/32148/explanation-of-the-formula-of-the-map-funtion
     return parseInt((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+}
+function diagnallength(elm) {
+    return (Math.sqrt(elm.offsetHeight * elm.offsetHeight + elm.offsetWidth * elm.offsetWidth)) || (Math.sqrt(elm.innerHeight * elm.innerHeight + elm.innerWidth * elm.innerWidth))
+}
+
+function direction(angle, radius) {
+    angle = (angle / 180) * Math.PI
+    return ({ x: Math.cos(angle) * radius, y: Math.sin(angle) * radius })
 }
